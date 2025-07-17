ad6c4b4f13f760e215c1049d605b8ef33f3ceea6
Matheus Amazonas <matheus.amazonas@gmail.com>
2025-07-17 22:23:19 +0200

    ✨ Add version fetching
    Via stdin, using a regex.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use regex::{self, Regex};
+use std::io;
 use std::process::{Command, Stdio};
 
 fn find_adb() -> bool {
@@ -8,11 +10,27 @@ fn find_adb() -> bool {
 		.is_ok()
 }
 
+fn get_version() -> Option<String> {
+	let regex = Regex::new(r"\b\d+\.\d+\b").ok()?;
+	let mut input = String::new();
+	io::stdin().read_line(&mut input).ok()?;
+	let m = regex.find(&input)?;
+	Some(String::from(m.as_str()))
+}
+
 fn main() {
 	if !find_adb() {
-		println!("ADB nout found.");
+		println!("ADB not found. Please ensure that ADB is installed.");
 		return;
 	}
 
-	println!("ADB found.");
+	let version = loop {
+		println!("Which version would you like to install?");
+		match get_version() {
+			Some(version) => break version,
+			None => print!("Wrong version format. Example: 5.1. "),
+		}
+	};
+
+	println!("Version: {version:?}");
 }
