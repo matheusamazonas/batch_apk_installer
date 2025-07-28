276fa3dad45a3366b0e517b98491fbe7ac0fce3b
Matheus Amazonas <matheus.amazonas@gmail.com>
2025-07-28 16:41:37 +0200

    🥅 Use the standard error stream for errors
    Separate regular output from error output so errors are always displayed on the terminal, and never sent to files.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
 	if !adb_installer::find_adb() {
-		println!("ADB not found. Please ensure that ADB is installed.");
+		eprintln!("ADB not found. Please ensure that ADB is installed.");
 		return;
 	}
 
@@ -8,7 +8,7 @@ fn main() {
 		println!("Which version would you like to install?");
 		match adb_installer::get_version() {
 			Some(version) => break version,
-			None => print!("Wrong version format. Example: 5.1. "),
+			None => eprint!("Wrong version format. Example: 5.1. "),
 		}
 	};
 
