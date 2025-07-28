2e805a6cdcba87ebf584e2d9f472bf2f74721248
Matheus Amazonas <matheus.amazonas@gmail.com>
2025-07-28 16:48:24 +0200

    ✨ Replace terminal input with command line argument

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
 use regex::{self, Regex};
-use std::io;
 use std::process::{Command, Stdio};
-
-pub struct Config {
-	version: String,
-}
 
 pub fn find_adb() -> bool {
 	Command::new("adb")
@@ -14,14 +9,8 @@ pub fn find_adb() -> bool {
 		.is_ok()
 }
 
-fn parse_version(input: &str) -> Option<String> {
+pub fn parse_version(input: &str) -> Option<String> {
 	let regex = Regex::new(r"\b\d+\.\d+\b").ok()?;
 	let result = regex.find(&input)?;
 	Some(String::from(result.as_str()))
-}
-
-pub fn get_version() -> Option<String> {
-	let mut input = String::new();
-	io::stdin().read_line(&mut input).ok()?;
-	parse_version(&input)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
+use std::env;
+use std::process;
+
 fn main() {
 	if !adb_installer::find_adb() {
 		eprintln!("ADB not found. Please ensure that ADB is installed.");
 		return;
 	}
 
-	let version = loop {
-		println!("Which version would you like to install?");
-		match adb_installer::get_version() {
-			Some(version) => break version,
-			None => eprint!("Wrong version format. Example: 5.1. "),
+	let args: Vec<String> = env::args().collect();
+	if args.len() < 2 {
+		eprintln!("Missing arguments: version.");
+		process::exit(1);
+	}
+
+	let version_arg = args[1].clone();
+	let version = match adb_installer::parse_version(&version_arg) {
+		Some(version) => version,
+		None => {
+			eprintln!("Wrong version format. Example: 5.1");
+			process::exit(1)
 		}
 	};
 
